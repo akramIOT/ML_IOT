0dfa59e5606720bfc0a61d161a0b5b296ff590c3
akramIOT <sheriff.akram@gmail.com>
2022-07-26 19:45:30 -0700

    PCA  technique from scratch using  numpy
    This is to do PCA  on a High dimension data with multiple  features  and  reduce the dimension by PCA method.

--- models/PCA_IOT.py
+++ models/PCA_IOT.py
@@ -70,8 +70,7 @@ if __name__ == "__main__":
     x2 = X_projected[:, 1]
 
     plt.scatter(
-        x1, x2, c=y, edgecolor="none", alpha=0.8, cmap=plt.cm.get_cmap("viridis", 3)
-    )
+        x1, x2, c=y, edgecolor="none", alpha=0.8, cmap=plt.cm.get_cmap("viridis", 3))
 
     plt.xlabel("Principal Component 1")
     plt.ylabel("Principal Component 2")
